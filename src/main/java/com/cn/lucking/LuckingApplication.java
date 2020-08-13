4717ccb05ccb57ad244b2d4ee90bc9c245ff9a7a
zhouhongtao-stack <john_hongtao@163.com>
2020-08-13 14:48:18 +0800

    lucking

---
+++ src/main/java/com/cn/lucking/LuckingApplication.java
@@ -0,0 +1,12 @@
+package com.cn.lucking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckingApplication.class, args);
+    }
+}
